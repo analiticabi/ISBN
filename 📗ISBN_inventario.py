28739a78faf4fe409ba604e95ab50d7e76d68174
analiticabi <analiticabi@grupo-sm.com>
2024-06-19 19:36:41 -0400

    Update 📗ISBN_inventario.py

--- 📗ISBN_inventario.py
+++ 📗ISBN_inventario.py
@@ -9,13 +9,13 @@ import numpy as np
 import pandas as pd
 #import matplotlib as pl
 #import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import silhouette_score
+#from sklearn.cluster import KMeans
+#from sklearn.preprocessing import StandardScaler
+#from sklearn.metrics import silhouette_score
 
 import pandas as pd 
 import numpy as np 
-import pickle 
+#import pickle 
 import streamlit as st 
 from PIL import Image 
 import time
